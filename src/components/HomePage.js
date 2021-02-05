a1567324ac225826af4d23f14919c7ac2e5eab50
evanson <igiri.evanson@gmail.com>
2021-02-05 14:34:06 +0100

    add the homepage/modal page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,31 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardActionArea, CardMedia, CardContent, Typography, Grid  } from '@material-ui/core';
+import { Card, CardActionArea, CardMedia, CardContent, Typography, Grid, Modal } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
   },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
 const HomePage = () => {
   const [hasError, setErrors] = useState(false);
-  const [airports, setAirports] = useState({});
+  const [airports, setAirports] = useState([]);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -26,34 +41,39 @@ const HomePage = () => {
 
   const classes = useStyles();
 
-  //console.log(airports)
- 
-
   return (
     <>
-     { airports.array.forEach(([col1, , col2, , , col3, col4, col5, , col6]) => {
-        const cols = [col1, col2, col3, col4, col5, col6];
-         <Grid item xs={4}>
-          <Card className={classes.root}>
-           <CardActionArea>
-             <CardMedia
-               component="img"
-               alt="Airport Images"
-               height="140"
-               image=""
-               title="Airport Images"
-             />
-             <CardContent>
-               {cols.map(c => (
-                 <Typography gutterBottom variant="h5" component="h2">
-                   {c.col2}
-                 </Typography>
-               ))}
-             </CardContent>
-           </CardActionArea>
-         </Card>
-       </Grid>
-      })}
+      <Grid container spacing={5}>
+        {airports.map(cols => (
+          <Grid item xs={6} sm={3} onClick={handleOpen}>
+            <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="Airport Images"
+                height="140"
+                image=""
+                title="Airport Images"
+              />
+              <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {cols[2]}
+                  </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        ))}
+      </Grid>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        className={classes.modal}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+       <h1>Hello</h1>
+      </Modal>
     </> 
   )
 }
